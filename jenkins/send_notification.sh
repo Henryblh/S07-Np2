cb3fd45eff0b40799003eed935305f091c31389e
Henryblh <henriquejunbicalho@gmail.com>
2026-06-05 20:19:38 -0300

    fix: corrigindo jenkinsfile e pipeline de email3

--- jenkins/send_notification.sh
+++ jenkins/send_notification.sh
@@ -3,7 +3,16 @@ TO_EMAIL="$NOTIFICATION_EMAIL"
 SUBJECT="Pipeline Status: $JOB_NAME - Build #$BUILD_NUMBER"
 BODY="Pipeline finished with status: $JOB_STATUS. See artifacts at $BUILD_URL"
 
-sendmail -S mailhog:1025 -f "jenkins@projetos07.com" "$TO_EMAIL" <<EOF
+# 1. Cria um arquivo de texto com o esqueleto do e-mail
+cat <<EOF > email.txt
+From: jenkins@projetos07.com
+To: $TO_EMAIL
 Subject: $SUBJECT
+
 $BODY
 EOF
+
+# 2. Usa o curl para injetar o e-mail direto na porta 1025 do MailHog
+curl smtp://mailhog:1025 --mail-from "jenkins@projetos07.com" --mail-rcpt "$TO_EMAIL" --upload-file email.txt
+
+echo "Notificação processada via cURL!"
